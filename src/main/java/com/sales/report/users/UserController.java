671c092f43c6ba82af983c5649779718d1b9a0eb
Romanlembak <lembak.roman@gmail.com>
2024-11-14 18:10:10 +0200

    created UserController with method signup

---
+++ src/main/java/com/sales/report/users/UserController.java
@@ -0,0 +1,23 @@
+package com.sales.report.users;
+
+import com.sales.report.users.model.UserDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class UserController {
+    private final UserService service;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody UserDTO userDTO) {
+        service.signup(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
