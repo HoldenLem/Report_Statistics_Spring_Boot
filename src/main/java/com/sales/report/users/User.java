4f4e3009dfecd4ec926b365ea768bcc9190864d8
Romanlembak <lembak.roman@gmail.com>
2024-11-14 15:33:15 +0200

    created UserRepository

--- src/main/java/com/sales/report/users/User.java
+++ src/main/java/com/sales/report/users/User.java
@@ -2,6 +2,8 @@ package com.sales.report.users;
 
 import lombok.*;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
@@ -10,7 +12,10 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+
+@Document
 public class User {
+    @Id
     private Long id;
     private String email;
     private String password;
